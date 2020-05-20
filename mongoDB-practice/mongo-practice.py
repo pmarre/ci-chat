11194bd9090d4f747c78f3aca9c71ec5c6b30569
Patrick Marre <pmarre40@gmail.com>
2020-05-20 15:17:36 -0700

    mongo practice

---
+++ mongoDB-practice/mongo-practice.py
@@ -0,0 +1,30 @@
+import pymongo
+import os
+from config import *
+
+
+MONGODB_URI = mongoDB
+DBS_NAME = "testDB"
+COLLECTION_NAME = "testMDB"
+
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connet to MongoDB: %s") % e
+
+
+conn = mongo_connect(MONGODB_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+coll.update_many({'nationality': 'american'}, {
+    '$set': {'hair_colour': 'maroon'}})
+
+documents = coll.find({'nationality': 'american'})
+
+for doc in documents:
+    print(doc)
